9f6a9c2aa23369925fb70e88a31563e5bb88faca
FabioBjr <fabiobritojunior1101@gmail.com>
2024-04-16 23:24:41 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,57 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
+public class Program {
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Account account;
+		
+		System.out.print("Enter account number: ");
+		int number = sc.nextInt();
+		System.out.print("Enter account holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		
+		System.out.print("Is there an initial deposite (y/n)? ");
+		char response = sc.next().charAt(0);
+		
+		if (response == 'y') {
+			System.out.print("Enter initial deposite value: ");
+			double initialDeposit = sc.nextDouble();
+			account = new Account(number, holder, initialDeposit);
+		}
+		else {
+			account = new Account(number, holder);
+		}
+		
+		System.out.println();
+		System.out.println("Account data:");
+		System.out.print(account);
+		
+		System.out.println();
+		System.out.print("Enter a deposit value: ");
+		double depositValue = sc.nextDouble();
+		account.deposit(depositValue);
+		System.out.println("Updated account data:");
+		System.out.print(account);
+		
+		System.out.println();
+		System.out.print("Enter a withdraw value: ");
+		depositValue = sc.nextDouble();
+		account.withdraw(depositValue);
+		System.out.println("Updated account data:");
+		System.out.print(account);
+		
+		
+		
+		
+		sc.close();
+	}
+}
